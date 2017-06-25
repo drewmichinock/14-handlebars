0be2d92917c0015c6b77429a106525372db14375
drewmichinock <drewmichinock@gmail.com>
2017-06-25 13:44:29 -0700

    5th commit

--- config/connection.js
+++ config/connection.js
@@ -25,8 +25,5 @@ connection.connect(function (err) {
 // export connection for orm to use
 module.exports = connection;
 
-// export connection for orm to use
-module.exports = mysql;
-
 // export router
 // module.exports = router;
